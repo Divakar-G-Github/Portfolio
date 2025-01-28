8bb851b4c6f2ac224753c490aaeea4eec572acea
Divakar-G-Github <divakarg.work@gmail.com>
2025-01-28 10:54:15 +0530

    Files upload

---
+++ src/data/projects.js
@@ -0,0 +1,29 @@
+import stonks from "../assets/images/stonks.png";
+import live from "../assets/images/live.jpg";
+import job from "../assets/images/job.jpg";
+import scroll from "../assets/images/scroll.jpg";
+import Wsn from "../assets/images/Wsn.jpg";
+
+export const projects = [
+  {
+    title: "WSNAP Using Supervised ML Tech",
+    image: Wsn,
+    link: "https://get-vulnerable.teshank.dev",
+    description:
+      "A Data-Driven Approach to Enhance WSN Security",
+  },
+  {
+    title: "Job Trend Analysis",
+    image: job,
+    link: "https://github.com/teshank2137/semicolon.md",
+    description:
+      "Job Trend Analysis explores market trends, skills, and salaries for better career insights.",
+  },
+  {
+    title: "Live Emoji Detection",
+    image: live,
+    link: "https://sendformto.me",
+    description:
+      "A real-time system that detects facial expressions as emojis.",
+  },
+];
